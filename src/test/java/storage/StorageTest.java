eb572403bc37bb32e99439a419e8eec28aac8f3e
racinmat <racinmat@fel.cvut.cz>
2018-04-10 15:08:37 +0200

    testing standard output

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -1,10 +1,13 @@
 package storage;
 
 import helpers.ItemStub;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import shop.Item;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 
@@ -13,10 +16,17 @@ import static org.junit.Assert.*;
 public class StorageTest {
 
     private Storage storage;
+    private ByteArrayOutputStream myOut = new ByteArrayOutputStream();
 
     @Before
     public void setUp() throws Exception {
         storage = new Storage();
+        System.setOut(new PrintStream(myOut));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(System.out);
     }
 
     @Test
@@ -35,5 +45,13 @@ public class StorageTest {
         HashMap<Integer, ItemStock> stock = (HashMap<Integer, ItemStock>) privateStockField.get(storage);
 
         assertEquals(1, stock.size());
+    }
+
+    @Test
+    public void testPrintListOfStoredItems() {
+        storage.printListOfStoredItems();
+        assertEquals("STORAGE IS CURRENTLY CONTAINING:\r\n", myOut.toString());
+//        System.out.println("já se vypíšu");
+//        assertEquals("já se vypíšu\r\n", myOut.toString());
     }
 }
